4365681f652ec4d2e73552365bfcd32844d3b468
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-05-16 18:27:54 +0530

    Implemented ProtectedRoute component for routing

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  console.log('ProtectedRoute check'); 
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
