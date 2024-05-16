798834cdd8d0eefee121b8374f0b86da88a9edad
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-05-16 18:30:51 +0530

    Integrated Redux state management into Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom'; 
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setCredentials } from '../store/authSlice';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const history = useHistory(); 
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setErrorMessage(''); 
+    setErrorMessage('');
     try {
       const res = await axios.post('http://localhost:3001/api/user/login', { username, password });
-      console.log(res.data);
-     
+      
+      dispatch(setCredentials({ user: username, token: res.data.token }));
       localStorage.setItem('token', res.data.token); 
-      history.push('/');
+      history.push('/dashboard'); 
     } catch (error) {
       if (error.response && error.response.data) {
-        setErrorMessage(error.response.data); 
+        setErrorMessage(error.response.data.message); 
       } else {
-        setErrorMessage('An unexpected error occurred.'); 
+        setErrorMessage('An unexpected error occurred.');
       }
     }
   };
