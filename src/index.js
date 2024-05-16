bb1ea8b11c667a8a0faee4091c32a8d3de14ffd9
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-05-16 19:06:05 +0530

    Integrated Redux store into the application using Provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
