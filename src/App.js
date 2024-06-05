1f65f8851d73cd372b0e55f7303d9ad587f12fa4
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-06-05 09:59:28 +0530

    Integrated Axios interceptors setup in App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
+import setupAxiosInterceptors from './axiosInterceptor';
 
 function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    setupAxiosInterceptors(history);
+  }, [history]);
+
   return (
     <div className="App">
       <Switch>
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <ProtectedRoute path="/dashboard" component={Dashboard} />
-        
       </Switch>
     </div>
   );
