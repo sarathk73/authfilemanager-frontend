cae1d8954ec233b02db70dff6f4385e35bbfc783
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-06-05 23:08:58 +0530

    Refactor: Update App component to include routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Switch, useHistory } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import FileUpload from './components/FileUpload';
+import FileDownload from './components/FileDownload';
+import CreateTask from './components/CreateTask';
+import GetAllTasks from './components/GetAllTasks';
 import ProtectedRoute from './components/ProtectedRoute';
 import setupAxiosInterceptors from './axiosInterceptor';
 
@@ -19,6 +23,10 @@ function App() {
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <ProtectedRoute path="/dashboard" component={Dashboard} />
+        <ProtectedRoute path="/tasks/create" component={CreateTask} />
+        <ProtectedRoute path="/tasks" component={GetAllTasks} exact />
+        <ProtectedRoute path="/upload" component={FileUpload} />
+        <ProtectedRoute path="/download" component={FileDownload} />
       </Switch>
     </div>
   );
